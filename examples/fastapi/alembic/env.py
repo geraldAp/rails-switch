6fc02c19d2f440cf0bed128009d613c347bf672f
geraldAp <amankwahprincegerald19@gmail.com>
2026-08-25 17:29:44 +0000

    refactor: Reorder import statements for better organization in env.py

--- examples/fastapi/alembic/env.py
+++ examples/fastapi/alembic/env.py
@@ -2,10 +2,11 @@ import asyncio
 from logging.config import fileConfig
 
 from alembic import context
-from app.core.config import settings
-from app.db.base import Base
 from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import async_engine_from_config
+
+from app.core.config import settings
+from app.db.base import Base
 
 config = context.config
 
